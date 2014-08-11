d13b21ff854b0eb901d95dd36ac3497033e1ab62
moiz786 <moiz.786@hotmail.com>
2014-08-11 19:01:15 +0500

    Adding cars to db and list cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,27 @@
 class CarsController < ApplicationController
 	before_filter :authenticate_user!
 
+	def index
+    @cars = Car.all
+  	end
 	def new
 		@car=Car.new
 	end
-	def show_used
-
+	def show
+		@car = Car.find(params[:id])
 	end
-	def show_new
-
-	end
+	
+	def create
+    @car = Car.new(car_params)
+ 
+    	if @car.save
+      		redirect_to @car
+    	else
+      		render 'new'
+    	end
+  	end
+  	private
+    def car_params
+      params.require(:car).permit(:car_type, :model, :make, :version)
+    end
 end
