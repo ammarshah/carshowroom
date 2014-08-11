d61c23a6cc178b75455bf726f0f4d1e338e521be
moiz786 <moiz.786@hotmail.com>
2014-08-11 15:01:23 +0500

    Adding Car controller with model having make,model,version,type as db fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807124850) do
+ActiveRecord::Schema.define(version: 20140811091137) do
+
+  create_table "cars", force: true do |t|
+    t.string   "car_type"
+    t.string   "make"
+    t.string   "model"
+    t.string   "version"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "name"
