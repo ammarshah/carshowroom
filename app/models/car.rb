f5eb4492b66504b56e4326bc3904eadfec8e3709
ammarshah <ammarshah123@gmail.com>
2014-08-20 20:25:55 +0500

    Finish Searchbar

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,11 @@
+require 'elasticsearch/model'
+
 class Car < ActiveRecord::Base
-	belongs_to :user
+  
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  belongs_to :user
 	has_many :photos, :dependent => :delete_all
 	validates :car_make,:car_model,:car_version,:car_year,:car_city,:car_mileage,:car_price,:car_trans,:car_desc,:car_type, presence: true
 
@@ -7,13 +13,41 @@ class Car < ActiveRecord::Base
   scope :new_cars, -> { where(:car_type => "New") }
   mount_uploader :image, ImageUploader
 
+  def self.search(query)
+  __elasticsearch__.search(
+      {
+       query: {
+          multi_match: {
+            query: query,
+              fields: ['car_*', 'car_make^2', 'car_model^3', 'car_version^4']
+          }
+        }
+      }
+    )
+  end
 
-  	def self.search(search)  
-    	if search
-      		where('car_make LIKE ?', "%#{search}%")
-      		# where('car_model LIKE ?', "%#{search}%")
-    	else
-      		@cars = Car.all
-    	end
-  	end
+  settings index: { number_of_shards: 1 } do
+  mappings dynamic: 'false' do
+    indexes :car_make, analyzer: 'english'
+    indexes :car_model, analyzer: 'english'
+    indexes :car_version, analyzer: 'english'
+    indexes :car_year, analyzer: 'english'
+    indexes :car_city, analyzer: 'english'
+    indexes :car_trans, analyzer: 'english'
+    indexes :car_desc, analyzer: 'english'
+    indexes :car_type, analyzer: 'english'
+    end
+  end
+
 end
+
+# Delete the previous cars index in Elasticsearch
+Car.__elasticsearch__.client.indices.delete index: Car.index_name rescue nil
+ 
+# Create the new index with the new mapping
+Car.__elasticsearch__.client.indices.create \
+  index: Car.index_name,
+  body: { settings: Car.settings.to_hash, mappings: Car.mappings.to_hash }
+ 
+# Index all car records from the DB to Elasticsearch
+Car.import
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
+  root to: 'visitors#index'  
   get "cars_all" => "cars#all", :as => "cars_all"
   get "cars_used_cars" => "cars#used_cars", :as => "used_cars"
   get "cars_new_cars" => "cars#new_cars", :as => "new_cars"
-  root to: 'visitors#index'
+  get 'search', to: 'search#search'
+
   devise_for :users
   resources :users
   resources :cars
